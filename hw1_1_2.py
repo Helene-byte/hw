817d30a28e477271adca1658f5e7712e0a3c8173
Olena Krasylshchykova <olena_krasylshchykova@epam.com>
2021-10-24 20:53:34 +0300

    script updated based on learn logs

--- hw1_1_2.py
+++ hw1_1_2.py
@@ -1,28 +1,36 @@
 import argparse
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("x", type=int, help="1st oprerand")
-parser.add_argument("z", help="sign")
-parser.add_argument("y", type=int, help="2nd operand")
+def calculate(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x", type=int, help="1st oprerand")
+    parser.add_argument("z", help="sign")
+    parser.add_argument("y", type=int, help="2nd operand")
 
 # parser.add_argument("-v", "--verbosity", action="count", default=0)
-args = parser.parse_args()
-try:
-    if '+' in args.z:
-        print( args.x+args.y)
-    elif '-' in args.z:
-        print(args.x-args.y)
-    elif '*' in args.z:
-        print(args.x*args.y)
+    args = parser.parse_args()
+    try:
+        if '+' in args.z:
+            return int(args.x+args.y)
+        elif '-' in args.z:
+            return args.x-args.y
+        elif '*' in args.z:
+            return args.x*args.y
 
-    elif '/' in args.z:
-        try:
-            print(args.x/args.y)
-        except:
-            e = sys.exc_info()[0]
-            print(e)
-except:
-    e = sys.exc_info()[0]
-    print("2", NotImplementedError, e)
+        elif '/' in args.z:
+            try:
+                return args.x/args.y
+            except:
+                e = sys.exc_info()[0]
+                print(e)
+    except:
+        e = sys.exc_info()[0]
+        print("2", NotImplementedError, e)
+def main():
+    args = None
+    print(calculate(args))
+
+
+if __name__ == '__main__':
+    main()
 
