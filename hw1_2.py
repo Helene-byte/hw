e3347902b77a76e1c517c8d26a2ef8043f525e47
Olena Krasylshchykova <olena_krasylshchykova@epam.com>
2021-10-25 20:25:59 +0300

    script updated based on learn logs

--- hw1_2.py
+++ hw1_2.py
@@ -1,18 +1,24 @@
 import argparse
-import sys
+import operator
 import math
 import operator
 
-parser = argparse.ArgumentParser()
-parser.add_argument("z", help="math/operator method")
-parser.add_argument("x", type=int, help="1st oprerand")
-parser.add_argument("y", type=int, help="2nd operand")
 
-# parser.add_argument("-v", "--verbosity", action="count", default=0)
-args = parser.parse_args()
-try:
-    print(getattr(operator,args.z)(args.x,args.y))
-except:
-    e = sys.exc_info()[0]
-    print(NotImplementedError, e)
+def calculate(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("operator", help="math/operator method")
+    parser.add_argument("operands", type=float, nargs="+", help="oprerands")
+    args = parser.parse_args()
+    c = getattr(math, args.operator, None) or getattr(operator, args.operator, None)
+    if c is None:
+        raise NotImplementedError()
+    print (c(args.operands[0]) if len(args.operands) == 1 else c(args.operands[0], args.operands[1]))
 
+
+def main():
+    args = None
+    print(calculate(args))
+
+
+if __name__ == '__main__':
+    main()
